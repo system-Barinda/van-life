59b9b1d35e559ac6a96eb87a7fabddf9dddec081
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-04 16:29:21 +0200

    correct/fix separate file into defferent card

--- src/pages/mobile/HostDashboard.jsx
+++ src/pages/mobile/HostDashboard.jsx
@@ -1,5 +1,6 @@
 import "../../styles/HostDashboard.css"
 import Navigation from "../../components/Navigation"
+import HostVanSectionCard from "../../components/HostVanSectionCard"
 export default function HostDashboard() {
   return (
     <div className="dashboard">
@@ -26,40 +27,9 @@ export default function HostDashboard() {
         <span className="link">Details</span>
       </section>
 
-      {/* Vans Section */}
-      <section className="vans">
-        <div className="vans-top">
-          <h3>Your listed vans</h3>
-          <span className="link">View all</span>
-        </div>
-
-        <div className="van-card">
-          <img src="/images/van1.png" alt="" />
-          <div>
-            <h4>Modest Explorer</h4>
-            <p>$60/day</p>
-          </div>
-          <span className="edit">Edit</span>
-        </div>
-
-        <div className="van-card">
-          <img src="/images/van2.png" alt="" />
-          <div>
-            <h4>Beach Bum</h4>
-            <p>$80/day</p>
-          </div>
-          <span className="edit">Edit</span>
-        </div>
-
-        <div className="van-card">
-          <img src="/images/van3.png" alt="" />
-          <div>
-            <h4>Green Wonder</h4>
-            <p>$70/day</p>
-          </div>
-          <span className="edit">Edit</span>
-        </div>
-      </section>
+      {/* HostVansSectionCard */}
+     
+      <HostVanSectionCard />
 
       {/* Footer */}
     
