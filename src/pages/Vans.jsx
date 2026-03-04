98f2302acf35a4497de23c62d23773d0a5aa7a2b
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-04 17:10:46 +0200

    done

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,23 +1,24 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "react-router-dom"
+import { VansContext } from "../context/VansContext"
 
 export default function Vans() {
-    const [vans, setVans] = React.useState([])
-    React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
+
+    const { vans } = useContext(VansContext)
+
+    if (!vans) return <p>Loading...</p>
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
-                <img src={van.imageUrl} />
+                <img src={van.imageUrl} alt={van.name} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
                     <p>${van.price}<span>/day</span></p>
                 </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                <i className={`van-type ${van.type} selected`}>
+                    {van.type}
+                </i>
             </Link>
         </div>
     ))
