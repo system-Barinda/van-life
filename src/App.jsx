86fbed1cb596f4fe16f1bd418f74ac5ffeb9f02e
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-04 12:45:34 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
       <Routes>
+               <Route element ={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+               </Route>
+
       </Routes>
     </BrowserRouter>
   )
