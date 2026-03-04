ff1315cda1b46351e5d4c00c76ea2b8a5d0bd879
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-04 17:14:07 +0200

    done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import "./service/server.js"
 import './index.css'
 import App from './App.jsx'
+import { VansProvider } from "./context/VansContext.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <VansProvider>
+      <App />
+    </VansProvider>
+  </StrictMode>
 )
