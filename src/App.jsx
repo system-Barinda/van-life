70b02f2afe51408c3202359d2b71831bb82f5421
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-04 12:57:43 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Layout from './components/Layout';
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
 
 function App() {
   return (
