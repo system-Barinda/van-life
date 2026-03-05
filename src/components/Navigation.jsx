2e697e48fad56bab63b0568a309dba83a744b6fe
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-05 12:08:11 +0200

    done

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ export default function Navigation() {
   return (
     <>
       <nav className="nav">
-        <NavLink className={activeClass} to="/Host">Dashboard</NavLink>
+        <NavLink className={activeClass} to="/Host" end>Dashboard</NavLink>
         <NavLink className={activeClass} to="/Host/Income">Income</NavLink>
         <NavLink className={activeClass} to="/Host/Vans">Vans</NavLink>
         <NavLink className={activeClass} to="/Host/HostReviews">Reviews</NavLink>
