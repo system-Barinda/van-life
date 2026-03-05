f35e2f52a59876ebad3a7fded349da9be4629e73
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-05 11:58:50 +0200

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link,NavLink } from "react-router-dom"
+
 
 export default function Header() {
+    const activeStyle = ({ isActive }) =>
+  isActive ? "isActiveheaderMenu" : ""
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink className={(isActiveHeaderMenu) => isActiveHeaderMenu ? "isActiveheaderMenu" : null } to="/Host">Host</NavLink>
-                <NavLink className={(isActiveHeaderMenu) => isActiveHeaderMenu ? "isActiveheaderMenu" : null } to="/about">About</NavLink>
-                <NavLink className={(isActiveHeaderMenu) => isActiveHeaderMenu ? "isActiveheaderMenu" : null } to="/vans">Vans</NavLink>
-            </nav>
+               <NavLink className={activeStyle} to="/host">Host</NavLink>
+               <NavLink className={activeStyle} to="/about">About</NavLink>
+               <NavLink className={activeStyle} to="/vans">Vans</NavLink>
+               </nav>
         </header>
     )
 }
