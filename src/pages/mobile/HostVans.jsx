980c139d78e21285a0fdf70bdfd1b40de619759e
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-05 13:04:45 +0200

    done

--- src/pages/mobile/HostVans.jsx
+++ src/pages/mobile/HostVans.jsx
@@ -4,26 +4,26 @@ import { VansContext } from "../../context/VansContext"
 import { useContext } from "react"
 export default function HostVans() {
 
-  const vans = useContext(VansContext)
-  
+  const {vans} = useContext(VansContext)
+
   return (
     <div className="host-page">
 
       <h2 className="title">Your listed vans</h2>
 
       <div className="vans-container">
-        {vans.map((van) => (
-          <div className="van-card" key={van.id}>
+       {vans?.map((van) => (
+  <Link to={`/host/vans/${van.id}`} className="van-card" key={van.id}>
+      <img src={van.imageUrl} alt={van.name} />
 
-            <img src={van.imageUrl} alt={van.name} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>${van.price}/day</p>
+      </div>
 
-            <div className="van-info">
-              <h3>{van.name}</h3>
-              <p>${van.price}/day</p>
-            </div>
-           <div className="arrow">➡️</div>
-          </div>
-        ))}
+      <div className="arrow">➡️</div>
+  </Link>
+))}
       </div>
 
     </div>
