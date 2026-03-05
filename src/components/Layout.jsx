6071833ff134d0350d818ae2ea28e48a6057794a
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-05 10:16:18 +0200

    done to change the layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
+import Footer from "./Footer"
 
 export default function Layout() {
     return (
-        <>
-       <Header />
-       <Outlet />
-       </>
-
+        <div className="site-wrapper">
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     )
 }
